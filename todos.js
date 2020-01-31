7e626fc8c3bd8e8cdd51458144c8495017ec05b0
Carla Paiva <carla.paiva05@hotmail.com>
2020-01-30 21:15:50 -0300

    Funcionamento básico de um toDo

---
+++ todos.js
@@ -0,0 +1,48 @@
+var listTodos = document.querySelector('#app ul');
+var inputElement = document.querySelector('#app input');
+var buttonElement = document.getElementById('btnAdd');
+
+var todos = [
+    'Fazer café',
+    'Estudar Javascript',
+    'Publicar projetos no GitHub'
+];
+
+function renderToDos(){
+    listTodos.innerHTML = '';
+    for (todo of todos){
+        var tempElement = document.createElement('li');
+        var content = document.createTextNode(todo);
+
+        var linkElement = document.createElement('a');
+        var linkText = document.createTextNode('Excluir');
+        linkElement.appendChild(linkText);
+        linkElement.setAttribute('href', '#');
+
+        var pos = todos.indexOf(todos);
+        linkElement.setAttribute('onclick', 'deleteToDo(' + pos + ')')
+
+        listTodos.appendChild(tempElement);
+        listTodos.appendChild(content);
+        listTodos.appendChild(linkElement);
+    }
+}
+
+renderToDos();
+
+function addTodo(){
+    var todoText = inputElement.value;
+
+    if (todoText !== ''){
+        todos.push(todoText);
+        inputElement.value = '';
+        renderToDos();
+    }
+}
+
+buttonElement.addEventListener('click', addTodo);
+
+function deleteToDo(pos){
+    todos.splice(pos, 1);
+    renderToDos();
+}
